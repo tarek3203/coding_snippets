058367b214135c3e2cfbd5a77220dc64f5d30308
Tariq Bin Bashar <90054387+tarek3203@users.noreply.github.com>
2022-03-06 00:42:02 +0600

    Create without bits.cpp

---
+++ without bits.cpp
@@ -0,0 +1,52 @@
+/****************************************************************/
+/****************************************************************/
+/**                                                            **/
+/**                  BISMILLAHIR RAHMANIR RAHIM                **/
+/**                ENLIVEN___BOY___TAREK   NEWBIE              **/
+/**              MANARAT__lNTERNATIONAL__UNIVERSITY            **/
+/**       DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING       **/
+/**     INSHA-ALLAH  I WILL BE ABLE TO CONFIRM MY DREAM JOB    **/
+/**                                                            **/
+/****************************************************************/
+/****************************************************************/
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string.h>
+using namespace std;
+#define ll long long
+#define w(aa) ll tc;cin>>tc;while(tc--)
+#define endl '\n'
+const int N = 1e5 + 9, mod = 1e9 + 7;
+
+int main(){
+/*
+ios_base::sync_with_stdio(0);
+cin.tie(0)
+*/
+
+return 0;
+}
